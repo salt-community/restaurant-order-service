4af44e430bfe765239c9962bdc937c67259fbac6
Daniel Sandstrom <github@danielsandstrom.org>
2025-08-29 10:29:58 +0200

    init commit

---
+++ src/test/java/com/example/restaurantorderservice/RestaurantOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restaurantorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantOrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
