c50b668e03da87a8fcc799e364edfe72250ca87d
Daniel Sandstrom <github@danielsandstrom.org>
2025-09-01 15:21:53 +0200

    Added KafkaMessage record

---
+++ src/main/java/com/example/restaurantorderservice/outbox/KafkaMessage.java
@@ -0,0 +1,24 @@
+package com.example.restaurantorderservice.outbox;
+
+import com.example.restaurantorderservice.enums.OrderStatus;
+import com.example.restaurantorderservice.model.Item;
+import com.example.restaurantorderservice.model.Order;
+import jakarta.persistence.*;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
+
+public record KafkaMessage(
+    UUID orderId, OrderStatus orderStatus, Instant createdAt, double totalPrice, List<Item> items
+) {
+    public static KafkaMessage fromOrder(Order order) {
+        return new KafkaMessage(
+            order.getOrderId(),
+            order.getOrderStatus(),
+            order.getCreatedAt(),
+            order.getTotalPrice(),
+            order.getItems()
+        );
+    }
+}
