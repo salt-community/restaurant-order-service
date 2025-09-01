c3ecc3163f5b62f5770a4c6ec74b51bcf49659db
AmandaEmilia <amanda.e.strom@gmail.com>
2025-09-01 10:41:46 +0200

    Moved KafkaListner to OrderService

--- src/main/java/com/example/restaurantorderservice/config/KafkaConfig.java
+++ src/main/java/com/example/restaurantorderservice/config/KafkaConfig.java
@@ -1,44 +1,12 @@
 package com.example.restaurantorderservice.config;
 
-import com.example.restaurantorderservice.dto.kafka.KafkaOrderDto;
-import com.example.restaurantorderservice.model.Order;
-import com.example.restaurantorderservice.repository.OrderRepository;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.kafka.clients.admin.NewTopic;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
 public class KafkaConfig {
-
-    private final ObjectMapper mapper;
-    private final OrderRepository orderRepository;
-
-    public KafkaConfig(OrderRepository orderRepository) {
-        this.orderRepository = orderRepository;
-        this.mapper = new ObjectMapper();
-    }
-
-    @KafkaListener(id = "myId", topics = "orders")
-    public void listen(ConsumerRecord<String, String> record) {
-        // injecting ConsumerRecord in case we need message metadata
-        // otherwise can just inject a String
-        String value = record.value();
-        try {
-            KafkaOrderDto dto = mapper.readValue(value, KafkaOrderDto.class);
-            Order order = dto.toOrder();
-            System.out.println("order id = " + order.getOrderId());
-            System.out.println("order status = " + order.getOrderStatus());
-            orderRepository.save(order);
-        } catch (JsonProcessingException e) {
-            System.out.println("EXCEPTION DESERIALIZING MESSAGE!");
-            e.printStackTrace();
-        }
-    }
 
     @Bean
     public NewTopic topic() {
--- src/main/java/com/example/restaurantorderservice/service/OrderService.java
+++ src/main/java/com/example/restaurantorderservice/service/OrderService.java
@@ -4,7 +4,11 @@ import com.example.restaurantorderservice.dto.kafka.KafkaOrderDto;
 import com.example.restaurantorderservice.dto.request.OrderRequestDto;
 import com.example.restaurantorderservice.model.Order;
 import com.example.restaurantorderservice.repository.OrderRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
@@ -14,10 +18,29 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     private final KafkaTemplate<String, KafkaOrderDto> kafkaTemplate;
+    private final ObjectMapper mapper;
 
     public void createOrder(OrderRequestDto req) {
         Order order = req.toOrder();
         orderRepository.save(order);
         kafkaTemplate.send("orders", KafkaOrderDto.fromOrder(order));
+    }
+
+    @KafkaListener(id = "myId", topics = "orders")
+    public void listen(ConsumerRecord<String, String> record) {
+        // injecting ConsumerRecord in case we need message metadata
+        // otherwise can just inject a String
+        String value = record.value();
+        try {
+            KafkaOrderDto dto = mapper.readValue(value, KafkaOrderDto.class);
+            Order order = dto.toOrder();
+            System.out.println("order id = " + order.getOrderId());
+            System.out.println("order status = " + order.getOrderStatus());
+            orderRepository.save(order);
+        } catch (JsonProcessingException e) {
+
+            System.out.println("EXCEPTION DESERIALIZING MESSAGE!");
+            e.printStackTrace();
+        }
     }
 }
