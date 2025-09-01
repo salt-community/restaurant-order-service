a07630695e1b2ef94caef96bb197ed31b39cbc91
Daniel Sandstrom <github@danielsandstrom.org>
2025-09-01 15:23:28 +0200

    Sketch of a stream for converting payload format from Order to Kafka message

--- src/main/java/com/example/restaurantorderservice/outbox/OutboxWorker.java
+++ src/main/java/com/example/restaurantorderservice/outbox/OutboxWorker.java
@@ -1,5 +1,7 @@
 package com.example.restaurantorderservice.outbox;
 
+import com.example.restaurantorderservice.model.Order;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -14,11 +16,24 @@ public class OutboxWorker {
     private final OutboxRepository outboxRepository;
 
     private final KafkaTemplate<String, String> kafkaTemplate;
+    private final ObjectMapper objectMapper;
 
     @Scheduled(fixedDelay = 500)
     public void publishOutboxEvents() {
         
         List<OutboxEvent> outboxEvents = outboxRepository.findAllUnprocessed();
+        // OutboxEvents -> Orders -> KafkaMessages -> Json string
+        outboxEvents.stream()
+            .map(outboxEvent -> {
+//                    objectMapper.readValue(outboxEvent.getPayload(), Order.class)
+                    return new Order();
+                }
+            )
+            .map(KafkaMessage::fromOrder)
+            .map(kafkaMessage -> {
+                return "SOME JSON STRING";
+            })
+            .toList();
 
         for (OutboxEvent event: outboxEvents) {
             System.out.println("eveneeeeeeeeeeet = " + event);
@@ -27,9 +42,6 @@ public class OutboxWorker {
             System.out.println("eveneeeeeeeeeeet = " + event);
             outboxRepository.save(event);
         }
-
-
-
 
     }
 }
