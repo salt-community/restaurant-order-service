6c8127b02f235572a8b94c52d49d12c118c3403f
AmandaEmilia <amanda.e.strom@gmail.com>
2025-09-08 15:29:40 +0200

    updating dto for PaymentFailed

--- src/main/java/com/example/restaurantorderservice/messaging/eventDto/PaymentFailedDto.java
+++ src/main/java/com/example/restaurantorderservice/messaging/eventDto/PaymentFailedDto.java
@@ -2,20 +2,12 @@ package com.example.restaurantorderservice.messaging.eventDto;
 
 import lombok.Builder;
 
-import java.math.BigDecimal;
-import java.time.Instant;
 import java.util.UUID;
 
 @Builder
 public record PaymentFailedDto(
     UUID eventId, // unique id for the message
-    UUID paymentId, // Payment.id
     String orderId,
-    BigDecimal amount,
-    String providerPaymentId,
-    String paymentStatus, // AUTHORIZED | FAILED | REFUNDED
-    String failureReason, // null unless FAILED
-    Instant createdAt, // original payment timestamp
-    Instant occurredAt // when we produced the event
+    String paymentStatus // AUTHORIZED | FAILED | REFUNDED
 ) {
 }
